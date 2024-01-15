c5f3f42f2b25a3937a1a4c4733b7dc9108f735fd
sms6874 <156629716+sms6874@users.noreply.github.com>
2024-01-15 13:33:12 +0900

    Update sketch_jan15a.ino

--- sketch_jan15a.ino
+++ sketch_jan15a.ino
@@ -1,9 +1,47 @@
-void setup() {
-  
+#include<Servo.h>
 
+Servo myservo;
+
+void setup(){
+  myservo.attach(13);
 }
 
-void loop() {
-  
+void loop(){
+  myservo.write(90);
+  delay(1000);
+  myservo.write(180);
+  delay(1000);
+}
+----------------------------------------------------
+int Sensor=10;
+int val;
+void setup(){
+  Serial.begin(9600);
+  pinMode(Sensor,INPUT);
+}
 
+void loop(){
+  val=digitalRead(Sensor);
+
+  if(val==0){
+    Serial.println("STOP!!");
+  }
+  else{
+    Serial.println("Nothing");
+  }
+}
+---------------------------------------------------
+int Buzzer_Pin=7;
+
+void setup(){
+  pinMode(Buzzer_Pin, OUTPUT);
+}
+
+void loop(){
+  tone(Buzzer_Pin, 523);
+  delay(1000);
+  tone(Buzzer_Pin, 587);
+  delay(1000);
+  tone(Buzzer_Pin, 659);
+  delay(1000);
 }
